0dd6b3b0c103a16fb60dde32a081c5eb22345576
Cleazyy <cleazy2002@gmail.com>
2023-08-25 17:05:35 +0200

    create backend container

--- destroy.sh
+++ destroy.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
-docker stop $(docker ps -aq) 2>/dev/null
-docker rm $(docker ps -aq) 2>/dev/null
-docker rmi $(docker images -aq) 2>/dev/null
-docker volume prune -f 2>/dev/null
-docker network prune -f 2>/dev/null
+docker stop $(docker ps -qa) 2>/dev/null
+docker rm $(docker ps -qa) 2>/dev/null
+docker rmi -f $(docker images -qa) 2>/dev/null
+docker volume rm $(docker volume ls -q) 2>/dev/null
+docker network rm $(docker network ls -q) 2>/dev/null
+docker system prune -a --volume 2>/dev/null
+docker system prune -a --force 2>/dev/null
