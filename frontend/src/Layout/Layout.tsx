a8275ccead98498a794b3a1888c735da5c00cef5
Cleazyy <cleazy2002@gmail.com>
2023-08-29 12:39:33 +0200

    init

---
+++ frontend/src/Layout/Layout.tsx
@@ -0,0 +1,16 @@
+import Footer from './Footer/Footer'
+import Header from './Header/Header'
+
+function Layout() {
+  return (
+    <>
+      <Header />
+      <main>
+          <h1>Layout</h1>
+      </main>
+      <Footer />
+    </>
+  )
+}
+
+export default Layout
