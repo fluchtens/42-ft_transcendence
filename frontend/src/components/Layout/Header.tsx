8655c0f160f8814e573417140c706b146a8342a8
Cleazyy <cleazy2002@gmail.com>
2023-09-01 16:50:09 +0200

    update header

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -1,24 +1,28 @@
-import { useLocation, Link } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom"
+import pongLogo from '/pong_logo.png'
 
 function Header() {
   const { pathname } = useLocation()
 
   return (
     <>
-      <header className="bg-secondary border-b border-quaternary text-white">
-        <nav className="p-6 flex items-center justify-between">
-          <div className="flex gap-3">
-            <a href="/" className="font-semibold">ft_transcendence</a>
+      <header className="bg-secondary border-b border-quaternary">
+        <nav className="p-3 container mx-auto flex max-w-screen-xl justify-between">
+          <div className="flex gap-3 items-center text-lg font-medium">
+            <img src={pongLogo} className="h-10 w-10"/>
+            <Link to={'/'}>ft_transcendence</Link>
           </div>
-          <div className="flex gap-4">
-            <div className={pathname === "/" ? "text-quaternary" : "text-white"}>
+          <div className="flex gap-4 items-center">
+            <div className={pathname === "/" ? "text-quaternary" : ""}>
               <Link to={'/'}>Home</Link>
             </div>
-            <div className={pathname === "/about" ? "text-quaternary" : "text-white"}>
+            <div className={pathname === "/about" ? "text-quaternary" : ""}>
               <Link to={'/about'}>About</Link>
             </div>
-            <div className={pathname === "/login" ? "text-quaternary" : "text-white"}>
-              <Link to={'/login'}>Login</Link>
+            <div className="text-white font-medium">
+              <Link to={'/login'}>
+               <button className="px-2.5 py-1.5 rounded-md bg-quinary hover:bg-quaternary">Sign in</button>
+              </Link>
             </div>
           </div>
         </nav>
