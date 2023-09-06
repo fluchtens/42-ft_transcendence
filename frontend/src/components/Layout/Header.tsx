f6c3fa8e4a8a6da1241ea6c7b3bc0fbab1c7ab77
Cleazyy <cleazy2002@gmail.com>
2023-09-06 20:14:26 +0200

    header button for mobile first responsive

--- frontend/src/components/Layout/Header.tsx
+++ frontend/src/components/Layout/Header.tsx
@@ -1,39 +1,49 @@
+import { useState } from 'react';
 import { useLocation, Link } from 'react-router-dom'
-import { AiFillHome } from 'react-icons/ai';
+import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { IoGameController } from 'react-icons/io5';
 import { GiPingPongBat } from "react-icons/gi";
 import { MdLeaderboard } from "react-icons/md";
 
 function Header() {
   const { pathname } = useLocation()
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(isMenuOpen == false);
+  };
 
   return (
     <>
       <header className="bg-secondary shadow-lg">
-        <nav className="flex items-center justify-between px-6 py-3">
+        <nav className="w-full flex flex-wrap items-center justify-between px-6 py-3">
       
           <div className="flex items-center">
-            <GiPingPongBat className="hidden md:w-7 md:h-7 md:inline md:mr-2.5"/>
+            <GiPingPongBat className="w-7 h-7 mr-2.5"/>
             <Link to={'/'} className="text-lg font-medium">ft_transcendence</Link>
           </div>
 
-          <ul className="hidden md:uppercase md:font-medium md:flex md:items-center md:gap-4">
-            <li className={pathname === "/" ? "text-tertiary flex items-center" : "flex items-center"}>
+          <button className="hover:text-white md:hidden"onClick={toggleMenu}>
+            <AiOutlineMenu className="w-6 h-6"/>
+          </button>
+
+          <ul className={`${!isMenuOpen ? "hidden" : "flex flex-col w-full py-2 gap-1"} font-medium md:flex md:flex-row md:w-max md:uppercase md:items-center md:py-0 md:gap-4`}>
+            <li className={`${pathname === "/" ? "text-tertiary" : ""} flex items-center`}>
               <AiFillHome className="w-5 h-5 mr-1.5 mb-0.5" />
-              <Link to={'/'} className="hover:text-tertiary">Home</Link>
+              <Link to={'/'} className="hover:text-tertiary ">Home</Link>
             </li>
 
-            <li className={pathname === "/game" ? "text-tertiary flex items-center" : "flex items-center"}>
+            <li className={`text- ${pathname === "/game" ? "text-tertiary" : ""} flex items-center`}>
               <IoGameController className="w-5 h-5 mr-1.5 mb-0.5" />
               <Link to={'/game'} className="hover:text-tertiary">Game</Link>
             </li>
 
-            <li className={pathname === "/leaderboard" ? "text-tertiary flex items-center" : "flex items-center"}>
+            <li className={`${pathname === "/leaderboard" ? "text-tertiary" : ""} flex items-center`}>
               <MdLeaderboard className="w-5 h-5 mr-1.5 mb-0.5" />
               <Link to={'/leaderboard'} className="hover:text-tertiary">Leaderboard</Link>
             </li>
 
-            <li className="text-white">
+            <li className="text-white mt-1 md:mt-0">
               <Link to={'/login'}>
                 <button className="uppercase rounded-md bg-quaternary hover:bg-tertiary px-2.5 py-1.5">Sign in</button>
               </Link>
