c946f68caea92bcd3527c57996f9efe21a014991
Cleazyy <cleazy2002@gmail.com>
2023-10-15 18:21:15 +0200

    update env variables

--- backend/src/auth/strategies/42.strategy.ts
+++ backend/src/auth/strategies/42.strategy.ts
@@ -2,25 +2,18 @@ import { Strategy } from 'passport-42';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class FortyTwoStrategy extends PassportStrategy(Strategy, '42') {
   constructor(configService: ConfigService) {
     super({
-      clientID:
-        'u-s4t2ud-f80131a9603828942f8033274376e698e8b767d2480d22e69afa464157bb4ce2',
-      clientSecret:
-        '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',
-      callbackURL: 'http://localhost:3000/auth/42Auth',
+      clientID: configService.get('FORTY_TWO_UID'),
+      clientSecret: configService.get('FORTY_TWO_SECRET'),
+      callbackURL: configService.get('VITE_BACK_URL') + '/auth/42Auth',
     });
   }
 
-  async validate(
-    accessToken: string,
-    refreshToken: string,
-    profile: any,
-  ): Promise<any> {
+  async validate(accessToken: string, refreshToken: string, profile: any) {
     return profile;
   }
 }
