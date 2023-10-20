db3f1d6dd659b68ef67b1636445b3f20dee1f2a1
Cleazyy <cleazy2002@gmail.com>
2023-10-20 09:29:41 +0200

    fix uploads folder

--- backend/src/user/middlewares/multer.options.ts
+++ backend/src/user/middlewares/multer.options.ts
@@ -1,10 +1,16 @@
 import { User } from '@prisma/client';
 import { diskStorage } from 'multer';
 import * as path from 'path';
+import * as fs from 'fs';
+
+const uploadDirectory = './uploads';
+if (!fs.existsSync(uploadDirectory)) {
+  fs.mkdirSync(uploadDirectory);
+}
 
 export const multerAvatarOptions = {
   storage: diskStorage({
-    destination: './uploads',
+    destination: uploadDirectory,
     filename: (req, file, callback) => {
       const user = req.user as User;
       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
