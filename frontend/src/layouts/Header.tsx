764ea9a5aca604a8dd6707615d16208220db5a2b
Cleazyy <cleazy2002@gmail.com>
2023-10-24 11:39:15 +0200

    add reponsive button for nav links header

--- frontend/src/layouts/Header.tsx
+++ frontend/src/layouts/Header.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { MainLink } from "../components/MainLink";
+import { MainNavLink } from "../components/MainNavLink";
 import { NavLink } from "../components/NavLink";
 import { ProfileBtn } from "../components/ProfileBtn";
 import { User } from "../types/user.interface";
@@ -11,9 +11,14 @@ import { BsFillChatDotsFill } from "react-icons/bs";
 import styles from "./Header.module.scss";
 
 export default function Header() {
+  const [navMenu, setNavMenu] = useState<boolean>(false);
   const [user, setUser] = useState<User | null>(null);
   const [avatar, setAvatar] = useState<string>("");
   const navigate = useNavigate();
+
+  const toggleNavMenu = () => {
+    setNavMenu(!navMenu);
+  };
 
   const handleLogout = async () => {
     setUser(null);
@@ -37,20 +42,20 @@ export default function Header() {
   return (
     <header>
       <nav className={styles.navBar}>
-        <ul className={styles.navList}>
-          <li>
-            <MainLink />
-          </li>
-          <li>
-            <NavLink path="/" text="HOME" icon={<AiFillHome />} />
-          </li>
-          <li>
-            <NavLink path="/game" text="GAME" icon={<IoGameController />} />
-          </li>
-          <li>
-            <NavLink path="/chat" text="CHAT" icon={<BsFillChatDotsFill />} />
-          </li>
-        </ul>
+        <div className={styles.links}>
+          <MainNavLink toggleNavMenu={toggleNavMenu} />
+          <ul className={`${navMenu ? styles.navListMenu : styles.navList}`}>
+            <li>
+              <NavLink path="/" text="HOME" icon={<AiFillHome />} />
+            </li>
+            <li>
+              <NavLink path="/game" text="GAME" icon={<IoGameController />} />
+            </li>
+            <li>
+              <NavLink path="/chat" text="CHAT" icon={<BsFillChatDotsFill />} />
+            </li>
+          </ul>
+        </div>
         {user ? (
           <ProfileBtn
             username={user.username}
