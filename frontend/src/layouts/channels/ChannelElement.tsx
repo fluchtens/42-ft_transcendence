844083a9550c347a1caf512df960487281d1152b
fluchtens <cleazy2002@gmail.com>
2023-11-22 09:08:49 +0100

    use navigate to channel element list

--- frontend/src/layouts/channels/ChannelElement.tsx
+++ frontend/src/layouts/channels/ChannelElement.tsx
@@ -1,3 +1,4 @@
+import { useNavigate } from "react-router-dom";
 import styles from "./ChannelElement.module.scss";
 
 interface ChannelElementProps {
@@ -6,9 +7,15 @@ interface ChannelElementProps {
 }
 
 const ChannelElement = ({ name, status }: ChannelElementProps) => {
+  const navigate = useNavigate();
+
+  const navigateToChannel = () => {
+    navigate("/chat/1");
+  };
+
   return (
     <div className={styles.container}>
-      <button className={styles.channelBtn}>
+      <button className={styles.channelBtn} onClick={navigateToChannel}>
         <div>
           <p className={styles.name}>{name}</p>
           <p className={styles.status}>{status}</p>
