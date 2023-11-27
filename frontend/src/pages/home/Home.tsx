76931ce0e23168e2d56e01f074967f0a35870663
Cleazyy <cleazy2002@gmail.com>
2023-11-27 17:06:33 +0100

    fix home page size

--- frontend/src/pages/home/Home.tsx
+++ frontend/src/pages/home/Home.tsx
@@ -4,8 +4,10 @@ import styles from "./Home.module.scss";
 function Home() {
   return (
     <div className={styles.container}>
-      <h1>Welcome to ft_transcendence</h1>
-      <h2>Final project of the common core curriculum at 42</h2>
+      <div className={styles.title}>
+        <h1>Welcome to ft_transcendence</h1>
+        <h2>Final project of the common core curriculum at 42</h2>
+      </div>
       <Link to="/game" className={styles.playButton}>
         Play Now
       </Link>
