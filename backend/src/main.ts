c8e1b847352487ca9f17729ca7d43c1f13050c08
Cleazyy <cleazy2002@gmail.com>
2024-01-27 15:51:11 +0100

    update main add httpsOptions

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,14 +3,26 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as cookieParser from 'cookie-parser';
 import * as session from 'express-session';
+import * as fs from 'fs';
+import * as path from 'path';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  // const httpsOptions = {
+  //   key: fs.readFileSync(path.join(__dirname, '../privkey.pem')),
+  //   cert: fs.readFileSync(path.join(__dirname, '../fullchain.pem')),
+  // };
+
+  const app = await NestFactory.create(AppModule, {
+    // httpsOptions,
+  });
+
   app.enableCors({
     origin: process.env.VITE_FRONT_URL,
     credentials: true,
   });
+
   app.useGlobalPipes(new ValidationPipe());
+
   app.use(
     cookieParser(),
     session({
