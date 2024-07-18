1eb65383d4053ef053f6a95082df5b1463721c6f
Cleazyy <cleazy2002@gmail.com>
2024-07-18 16:17:49 +0200

    add Montserrat to tailwind config font family

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -13,6 +13,9 @@ const config = {
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["Montserrat", "sans-serif"],
+      },
       colors: {
         border: "var(--border)",
         input: "hsl(var(--input))",
