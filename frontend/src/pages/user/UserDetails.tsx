d650c2fd6741348afc7c612b5d54489f7683b7ba
Cleazyy <cleazy2002@gmail.com>
2024-07-19 10:17:02 +0200

    update user details component

--- frontend/src/pages/user/UserDetails.tsx
+++ frontend/src/pages/user/UserDetails.tsx
@@ -1,15 +1,28 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "@/components/ui/separator";
 import { FaMedal, FaUserPen, FaUserPlus } from "react-icons/fa6";
 import { IoGameController, IoPodiumSharp } from "react-icons/io5";
 import { PiFootprintsFill } from "react-icons/pi";
-import { Separator } from "../../components/Separator";
 import { Stats } from "../../types/game.interface";
 import { User } from "../../types/user.interface";
 import { convertDate } from "../../utils/date";
 import winLossRatio from "../../utils/winLossRatio";
 import { ManageBtn } from "./ManageBtn";
 import { StatsBar } from "./StatsBar";
-import styles from "./UserDetails.module.scss";
+
+interface UserDetailElementProps {
+  icon: React.ReactNode;
+  title: string;
+  value: string | number;
+}
+
+const UserDetailElement = ({ icon, title, value }: UserDetailElementProps) => (
+  <li className="mb-2 py-2 flex items-center gap-2 border-b">
+    {icon}
+    <p className="text-sm font-medium whitespace-nowrap">{title}</p>
+    <p className="text-sm font-normal whitespace-nowrap ml-auto">{value}</p>
+  </li>
+);
 
 interface UserDetailsProps {
   targetUser: User;
@@ -20,10 +33,10 @@ const UserDetails = ({ targetUser, stats }: UserDetailsProps) => {
   const totalMatches = stats.wonMatches + stats.lostMatches;
 
   return (
-    <div className={styles.container}>
-      <h1 className="text-2xl font-semibold">Profile</h1>
-      <Separator />
-      <div className={styles.profile}>
+    <div>
+      <h1 className="text-2xl font-semibold text-center">Profile</h1>
+      <Separator className="mt-2" />
+      <div className="mt-6 flex flex-col md:flex-row justify-center items-center gap-12">
         <div className="flex flex-col items-center gap-3">
           <Avatar className="w-[14rem] h-[14rem] rounded-full">
             <AvatarFallback>{targetUser.username[0].toUpperCase()}</AvatarFallback>
@@ -32,41 +45,21 @@ const UserDetails = ({ targetUser, stats }: UserDetailsProps) => {
           <h2 className="text-xl font-semibold">{targetUser.username}</h2>
           <ManageBtn targetUser={targetUser} />
         </div>
-        <div className={styles.details}>
-          <div className={styles.lists}>
-            <ul>
-              <li className={styles.data}>
-                <PiFootprintsFill className={styles.icon} />
-                <p className={styles.type}>ID</p>
-                <p className={styles.value}>{targetUser.id}</p>
-              </li>
-              <li className={styles.data}>
-                <FaUserPlus className={styles.icon} />
-                <p className={styles.type}>Registered</p>
-                <p className={styles.value}>{convertDate(targetUser.createdAt)}</p>
-              </li>
-              <li className={styles.data}>
-                <FaUserPen className={styles.icon} />
-                <p className={styles.type}>Updated</p>
-                <p className={styles.value}>{convertDate(targetUser.updatedAt)}</p>
-              </li>
+        <div className="w-full flex flex-col gap-4">
+          <div className="flex flex-col md:flex-row md:gap-12">
+            <ul className="w-full">
+              <UserDetailElement icon={<PiFootprintsFill className="w-[1rem] h-[1rem]" />} title="ID" value={targetUser.id} />
+              <UserDetailElement icon={<FaUserPlus className="w-[1rem] h-[1rem]" />} title="Registered" value={convertDate(targetUser.createdAt)} />
+              <UserDetailElement icon={<FaUserPen className="w-[1rem] h-[1rem]" />} title="Updated" value={convertDate(targetUser.updatedAt)} />
             </ul>
-            <ul>
-              <li className={styles.data}>
-                <IoGameController className={styles.icon} />
-                <p className={styles.type}>Total games</p>
-                <p className={styles.value}>{totalMatches}</p>
-              </li>
-              <li className={styles.data}>
-                <FaMedal className={styles.icon} />
-                <p className={styles.type}>Rating</p>
-                <p className={styles.value}>{targetUser.rating}</p>
-              </li>
-              <li className={styles.data}>
-                <IoPodiumSharp className={styles.icon} />
-                <p className={styles.type}>W/L ratio</p>
-                <p className={styles.value}>{winLossRatio(stats.wonMatches, stats.lostMatches)}</p>
-              </li>
+            <ul className="w-full">
+              <UserDetailElement icon={<IoGameController className="w-[1rem] h-[1rem]" />} title="Total games" value={totalMatches} />
+              <UserDetailElement icon={<FaMedal className="w-[1rem] h-[1rem]" />} title="Rating" value={targetUser.rating} />
+              <UserDetailElement
+                icon={<IoPodiumSharp className="w-[1rem] h-[1rem]" />}
+                title="W/L ratio"
+                value={winLossRatio(stats.wonMatches, stats.lostMatches)}
+              />
             </ul>
           </div>
           <StatsBar stats={stats} />
--- frontend/src/utils/date.tsx
+++ frontend/src/utils/date.tsx
@@ -1,8 +1,4 @@
-export function convertDate(date: string | null | undefined) {
-  if (!date) {
-    return null;
-  }
-
+export function convertDate(date: string): string {
   const newDate = new Date(date);
   return newDate.toLocaleDateString("fr-FR", {
     day: "2-digit",
