33188740c5d3275ead2b1a6e31f2093364d60f76
Cleazyy <cleazy2002@gmail.com>
2024-07-29 09:27:49 +0200

    add text to profile in no games played

--- frontend/src/pages/user/UserHistory.tsx
+++ frontend/src/pages/user/UserHistory.tsx
@@ -11,30 +11,34 @@ const UserHistory = ({ history }: UserHistoryProps) => (
   <div>
     <h1 className="text-2xl font-semibold text-center">Match history</h1>
     <Separator className="mt-2" />
-    <ul>
-      {history?.map((match) => (
-        <li className="py-4 flex flex-col items-center border-b" key={match.id}>
-          <p className="text-sm font-medium text-[#7e848c]">{convertDate(match.finished)}</p>
-          <div className="flex items-center gap-5">
-            <div className="flex flex-col items-end">
-              <p className="text-lg font-semibold">{match.winner.username}</p>
-              <div className="flex items-center gap-1">
-                <p className="text-base font-normal">{match.winnerRatingAfter}</p>
-                <p className="text-base font-medium text-[#81b64c]">+{match.winnerRatingAfter - match.winnerRatingBefore}</p>
+    {history?.length === 0 ? (
+      <p className="mt-2 text-sm text-muted-foreground text-center">No games played</p>
+    ) : (
+      <ul>
+        {history.map((match) => (
+          <li className="py-4 flex flex-col items-center border-b" key={match.id}>
+            <p className="text-sm font-medium text-[#7e848c]">{convertDate(match.finished)}</p>
+            <div className="flex items-center gap-5">
+              <div className="flex flex-col items-end">
+                <p className="text-lg font-semibold">{match.winner.username}</p>
+                <div className="flex items-center gap-1">
+                  <p className="text-base font-normal">{match.winnerRatingAfter}</p>
+                  <p className="text-base font-medium text-[#81b64c]">+{match.winnerRatingAfter - match.winnerRatingBefore}</p>
+                </div>
+              </div>
+              <GiPingPongBat className="w-[2rem] h-[2rem]" />
+              <div className="flex flex-col items-start">
+                <p className="text-lg font-semibold">{match.loser.username}</p>
+                <div className="flex items-center gap-1">
+                  <p className="text-base font-normal">{match.loserRatingAfter}</p>
+                  <p className="text-base font-medium text-[#e02827]">{match.loserRatingAfter - match.loserRatingBefore}</p>
+                </div>
               </div>
             </div>
-            <GiPingPongBat className="w-[2rem] h-[2rem]" />
-            <div className="flex flex-col items-start">
-              <p className="text-lg font-semibold">{match.loser.username}</p>
-              <div className="flex items-center gap-1">
-                <p className="text-base font-normal">{match.loserRatingAfter}</p>
-                <p className="text-base font-medium text-[#e02827]">{match.loserRatingAfter - match.loserRatingBefore}</p>
-              </div>
-            </div>
-          </div>
-        </li>
-      ))}
-    </ul>
+          </li>
+        ))}
+      </ul>
+    )}
   </div>
 );
 
