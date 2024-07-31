baefd14382487cf609085c3c80583eeeb2100043
Cleazyy <cleazy2002@gmail.com>
2024-07-31 14:04:25 +0200

    update jwt token and cookie expiration time

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -5,19 +5,19 @@ import {
   NotFoundException,
   UnauthorizedException,
 } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
-import { RegisterDto } from './dtos/RegisterDto';
-import { LoginDto } from './dtos/LoginDto';
-import { SetupDto } from './dtos/SetupDto';
-import * as bcrypt from 'bcryptjs';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcryptjs';
 import * as fs from 'fs';
-import * as path from 'path';
 import fetch from 'node-fetch';
 import { authenticator } from 'otplib';
+import * as path from 'path';
 import { toDataURL } from 'qrcode';
-import { TwoFaDto } from './dtos/TwoFaDto';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { UserService } from 'src/user/user.service';
+import { LoginDto } from './dtos/LoginDto';
+import { RegisterDto } from './dtos/RegisterDto';
+import { SetupDto } from './dtos/SetupDto';
+import { TwoFaDto } from './dtos/TwoFaDto';
 
 @Injectable()
 export class AuthService {
@@ -31,19 +31,18 @@ export class AuthService {
   /*                                   Private                                  */
   /* -------------------------------------------------------------------------- */
 
-  private readonly cookieExpirationTime = 2 * 60 * 60 * 1000;
-
-  private async generateJwtToken(payload: any, expiresIn: string) {
+  private async generateJwtToken(payload: any) {
     const token = await this.jwtService.signAsync(payload, {
       secret: process.env.JWT_SECRET,
-      expiresIn: expiresIn,
+      expiresIn: '30d',
     });
 
     return token;
   }
 
-  private async setAccessTokenCookie(res, token: string, expiresIn: number) {
-    const expirationTime = new Date(Date.now() + expiresIn);
+  private async setAccessTokenCookie(res, token: string) {
+    const cookieExpirationTime = 30 * 24 * 60 * 60 * 1000;
+    const expirationTime = new Date(Date.now() + cookieExpirationTime);
 
     res.cookie('access_token', token, {
       httpOnly: true,
@@ -110,8 +109,8 @@ export class AuthService {
     }
 
     const payload = { id: user.id };
-    const token = await this.generateJwtToken(payload, '2h');
-    await this.setAccessTokenCookie(res, token, this.cookieExpirationTime);
+    const token = await this.generateJwtToken(payload);
+    await this.setAccessTokenCookie(res, token);
 
     return { message: 'User succesfully connected' };
   }
@@ -154,8 +153,8 @@ export class AuthService {
     }
 
     const payload = { id: user.id, username: user.username };
-    const token = await this.generateJwtToken(payload, '2h');
-    await this.setAccessTokenCookie(res, token, this.cookieExpirationTime);
+    const token = await this.generateJwtToken(payload);
+    await this.setAccessTokenCookie(res, token);
 
     return res.redirect(process.env.VITE_FRONT_URL);
   }
@@ -192,8 +191,8 @@ export class AuthService {
     }
 
     const payload = { id: user.id, username: user.username };
-    const token = await this.generateJwtToken(payload, '2h');
-    await this.setAccessTokenCookie(res, token, this.cookieExpirationTime);
+    const token = await this.generateJwtToken(payload);
+    await this.setAccessTokenCookie(res, token);
 
     res.clearCookie('connect.sid');
 
@@ -289,8 +288,8 @@ export class AuthService {
     }
 
     const payload = { id: userId };
-    const token = await this.generateJwtToken(payload, '2h');
-    await this.setAccessTokenCookie(res, token, this.cookieExpirationTime);
+    const token = await this.generateJwtToken(payload);
+    await this.setAccessTokenCookie(res, token);
 
     return { message: 'User succesfully connected' };
   }
